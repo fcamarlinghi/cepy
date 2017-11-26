d580839fc99d10d3536d317b038fc80dfe219139
Francesco Camarlinghi <me@minifloppy.it>
2017-11-26 16:28:21 +0100

    Update copyright year

--- bin/cepy.js
+++ bin/cepy.js
@@ -1,7 +1,7 @@
 ﻿//#!/usr/bin/env node
 
 /**
- * Copyright 2016 Francesco Camarlinghi
+ * Copyright 2016-2017 Francesco Camarlinghi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- cepy.js
+++ cepy.js
@@ -1,5 +1,5 @@
 ﻿/**
- * Copyright 2016 Francesco Camarlinghi
+ * Copyright 2016-2017 Francesco Camarlinghi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
